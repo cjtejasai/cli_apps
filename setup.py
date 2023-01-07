577b050a5b1a5d833f317d327d58172299f45269
cjtejasai <cjtejasai@gmail.com>
2023-01-08 00:49:53 +0530

    update setupfile

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     name='jarvis',
     version='0.1',
+    author="cjtejasai",
+    author_email="cjtejasai@gmail.com",
+    description="A command line interface for displaying system ",
     py_modules=['jarvis'],
     install_requires=[
         'click',
